d99c9a4da3ab861f4e8e9ab3d9b453de1b34f84c
Kevson Filipe <kevsonfilipesantos@gmail.com>
2023-03-28 10:45:57 -0300

    feat: added styled components

--- mobile/src/pages/Home/index.tsx
+++ mobile/src/pages/Home/index.tsx
@@ -1,145 +1,146 @@
-import React, { useState } from 'react';
-import { Feather as Icon } from '@expo/vector-icons';
-import { View, ImageBackground, Image, StyleSheet, Text, TextInput, KeyboardAvoidingView, Platform } from 'react-native';
-import { RectButton } from 'react-native-gesture-handler';
-import type { StackNavigationProp } from '@react-navigation/stack';
-import { useNavigation } from '@react-navigation/native';
+import { Feather as Icon } from "@expo/vector-icons";
+import { useNavigation } from "@react-navigation/native";
+import type { StackNavigationProp } from "@react-navigation/stack";
+import React, { useState } from "react";
+import { Image, ImageBackground, KeyboardAvoidingView, StyleSheet, Text, TextInput, View } from "react-native";
+import { RectButton } from "react-native-gesture-handler";
+import BackgroundSrc from "../../assets/home-background.png";
+import LogoSrc from "../../assets/logo.png";
+import * as S from "./styles";
 
 type RootStackParamList = {
-  Points: {    
+  Points: {
     uf: string;
     city: string;
   };
 };
 
 const Home = () => {
-  const [uf, setUf] = useState('');
-  const [city, setCity] = useState('');
+  const [uf, setUf] = useState("");
+  const [city, setCity] = useState("");
   const navigation = useNavigation<StackNavigationProp<RootStackParamList>>();
 
   function handleNavigateToPoints() {
-    navigation.navigate('Points', {
+    navigation.navigate("Points", {
       uf,
       city,
     });
   }
-  
-    return (
-      <KeyboardAvoidingView style={{ flex: 1 }} behavior="height">
-        <ImageBackground 
-            source={require('../../assets/home-background.png')} 
-            style={styles.container}
-            imageStyle={{ width: 274, height: 368 }}
-        >
-            <View style={styles.main}>
-               <Image source={require('../../assets/logo.png')}/>
-                <View>
-                <Text style={styles.title}>Seu marketplace de coleta de resíduos</Text>
-                <Text style={styles.description}>Ajudamos pesoas a encontrarem pontos de coletas de forma eficiente.</Text>
-               </View>
+
+  return (
+    <KeyboardAvoidingView style={{ flex: 1 }} behavior="height">
+      <ImageBackground
+        source={BackgroundSrc as number}
+        style={styles.container}
+        imageStyle={{ width: 274, height: 368 }}
+      >
+        <S.Main style={styles.main}>
+          <Image source={LogoSrc as number} />
+          <View>
+            <Text style={styles.title}>Seu marketplace de coleta de resíduos</Text>
+            <Text style={styles.description}>Ajudamos pesoas a encontrarem pontos de coletas de forma eficiente.</Text>
+          </View>
+        </S.Main>
+
+        <View style={styles.footer}>
+          <TextInput
+            style={styles.input}
+            placeholder="Digite a UF"
+            value={uf}
+            maxLength={2}
+            autoCapitalize="characters"
+            autoCorrect={false}
+            onChangeText={setUf}
+          />
+          <TextInput
+            style={styles.input}
+            placeholder="Digite a cidade"
+            value={city}
+            autoCorrect={false}
+            onChangeText={setCity}
+          />
+          <RectButton style={styles.button} onPress={handleNavigateToPoints}>
+            <View style={styles.buttonIcon}>
+              <Text>
+                <Icon name="arrow-right-circle" color="#FFF" size={24} />
+              </Text>
             </View>
-            
-            <View style={styles.footer}>
-                <TextInput
-                  style={styles.input}
-                  placeholder="Digite a UF"
-                  value={uf}
-                  maxLength={2}
-                  autoCapitalize="characters"
-                  autoCorrect={false}
-                  onChangeText={setUf}
-                />
-                <TextInput
-                  style={styles.input}
-                  placeholder="Digite a cidade"
-                  value={city}
-                  autoCorrect={false}
-                  onChangeText={setCity}
-                />
-                <RectButton style={styles.button} onPress={ handleNavigateToPoints }>
-                    <View style={styles.buttonIcon}>
-                        <Text>
-                            <Icon name="arrow-right-circle" color='#FFF' size={24}/>
-                        </Text>
-                    </View>
-                    <Text style={styles.buttonText}>
-                        Entrar
-                    </Text>
-                </RectButton>
-            </View>
-        </ImageBackground>
-        </KeyboardAvoidingView>
-    )
+            <Text style={styles.buttonText}>Entrar</Text>
+          </RectButton>
+        </View>
+      </ImageBackground>
+    </KeyboardAvoidingView>
+  );
 };
 
 const styles = StyleSheet.create({
-    container: {
-      flex: 1,
-      padding: 32,
-    },
-  
-    main: {
-      flex: 1,
-      justifyContent: 'center',
-    },
-  
-    title: {
-      color: '#322153',
-      fontSize: 32,
-      fontFamily: 'Ubuntu_700Bold',
-      maxWidth: 260,
-      marginTop: 64,
-    },
-  
-    description: {
-      color: '#6C6C80',
-      fontSize: 16,
-      marginTop: 16,
-      fontFamily: 'Roboto_400Regular',
-      maxWidth: 260,
-      lineHeight: 24,
-    },
-  
-    footer: {},
-  
-    select: {},
-  
-    input: {
-      height: 60,
-      backgroundColor: '#FFF',
-      borderRadius: 10,
-      marginBottom: 8,
-      paddingHorizontal: 24,
-      fontSize: 16,
-    },
-  
-    button: {
-      backgroundColor: '#34CB79',
-      height: 60,
-      flexDirection: 'row',
-      borderRadius: 10,
-      overflow: 'hidden',
-      alignItems: 'center',
-      marginTop: 8,
-    },
-  
-    buttonIcon: {
-      height: 60,
-      width: 60,
-      backgroundColor: 'rgba(0, 0, 0, 0.1)',
-      justifyContent: 'center',
-      alignItems: 'center',
-      borderRadius: 10
-    },
-  
-    buttonText: {
-      flex: 1,
-      justifyContent: 'center',
-      textAlign: 'center',
-      color: '#FFF',
-      fontFamily: 'Roboto_500Medium',
-      fontSize: 16,
-    }
-  });
+  container: {
+    flex: 1,
+    padding: 32,
+  },
+
+  main: {
+    flex: 1,
+    justifyContent: "center",
+  },
+
+  title: {
+    color: "#322153",
+    fontSize: 32,
+    fontFamily: "Ubuntu_700Bold",
+    maxWidth: 260,
+    marginTop: 64,
+  },
+
+  description: {
+    color: "#6C6C80",
+    fontSize: 16,
+    marginTop: 16,
+    fontFamily: "Roboto_400Regular",
+    maxWidth: 260,
+    lineHeight: 24,
+  },
+
+  footer: {},
+
+  select: {},
+
+  input: {
+    height: 60,
+    backgroundColor: "#FFF",
+    borderRadius: 10,
+    marginBottom: 8,
+    paddingHorizontal: 24,
+    fontSize: 16,
+  },
+
+  button: {
+    backgroundColor: "#34CB79",
+    height: 60,
+    flexDirection: "row",
+    borderRadius: 10,
+    overflow: "hidden",
+    alignItems: "center",
+    marginTop: 8,
+  },
+
+  buttonIcon: {
+    height: 60,
+    width: 60,
+    backgroundColor: "rgba(0, 0, 0, 0.1)",
+    justifyContent: "center",
+    alignItems: "center",
+    borderRadius: 10,
+  },
+
+  buttonText: {
+    flex: 1,
+    justifyContent: "center",
+    textAlign: "center",
+    color: "#FFF",
+    fontFamily: "Roboto_500Medium",
+    fontSize: 16,
+  },
+});
 
 export default Home;
---
+++ mobile/src/pages/Home/styles.ts
@@ -0,0 +1,6 @@
+import styled from "styled-components/native";
+
+export const Main = styled.View`
+  flex: 1;
+  justify-content: center;
+`;
